47616b12310dbc8eb72b64fb8f61301e179d0f31
MateChanadiri <mate.chanadiri@gmail.com>
2024-07-01 21:50:29 +0400

    my first projects

---
+++ calculator/calculator.py
@@ -0,0 +1,14 @@
+num1 = int(input("enter a number: "))
+num2 = int(input("enter a second number: "))
+plus = input("if you want to add enter + ")
+minus = input("if you want to minus enter - ")
+divide = input("if you want do divide enter / ")
+multiply = input("if you want to multiply enter * ")
+if plus:
+    print(num1 + num2)
+if minus:
+    print(num1 - num2)
+if divide:
+    print(num1 / num2)
+if multiply:
+    print(num1 * num2)
---
+++ new game hangman/new game hangman.py
@@ -0,0 +1,110 @@
+import random
+
+# List of the words
+words = ["house", "mate", "ilia", "tuta", "hello"]
+
+# Choosing the random word
+secret_word = random.choice(words).lower()
+
+# variables
+attempts = 6
+guessed_letters = []
+
+# Hangman stages
+hangman_stages = [
+    """
+       +---+
+       |   |
+           |
+           |
+           |
+           |
+     =======""",
+    """
+       +---+
+       |   |
+       O   |
+           |
+           |
+           |
+     =======""",
+    """
+       +---+
+       |   |
+       O   |
+       |   |
+           |
+           |
+     =======""",
+    """
+       +---+
+       |   |
+       O   |
+      /|   |
+           |
+           |
+     =======""",
+    """
+       +---+
+       |   |
+       O   |
+      /|\  |
+           |
+           |
+     =======""",
+    """
+       +---+
+       |   |
+       O   |
+      /|\  |
+      /    |
+           |
+     =======""",
+    """
+       +---+
+       |   |
+       O   |
+      /|\  |
+      / \  |
+           |
+     ======="""
+]
+
+#games loop
+
+while attempts > 0:
+    #displaying the hangman stage
+    print(hangman_stages[6 - attempts])
+
+    #displaying the status
+    display_word = ''.join([char if char in guessed_letters else '_' for char in secret_word])
+    print(display_word)
+
+    #checking if the word is guessed
+
+    if '_' not in display_word:
+        print("good job! you guessed the word that was:", secret_word)
+        break
+
+    #the code for input
+    guess = input("guess a letter:").lower()
+
+    #checking if the letter is alrady guessed
+    if guess in guessed_letters:
+        print("you guessed that letter already")
+        continue
+    guessed_letters.append(guess)
+
+    #checking if the letter is in the word
+    if guess not in secret_word:
+        attempts -= 1
+        print("incorrect guess :( you have",  attempts, "attempts left")
+    else:
+        print("correct guess nice")
+
+        #game over!
+
+        if attempts == 0:
+            print("game over! the word was:", secret_word)
+            
+
