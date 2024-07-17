1fdd51611c0dcc95ed5a56761509d5932b7e58aa
MateChanadiri <mate.chanadiri@gmail.com>
2024-07-17 13:04:38 +0400

    secondcommit

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -1,5 +1,5 @@
 def chatbot():
-    print("Hello, im a simple chatbot! can you enter your name for me please? i only ask 3 questions!")
+    print("Hello, im a simple chatbot! can you enter your name for me please? i only ask 5 questions!")
     user_name = input("you: ")
     print(f"Well hello there {user_name}, how are you?")
 
@@ -25,5 +25,23 @@ def chatbot():
 
     else:
         print("oh thats great to hear but are you sure youre that old?")
+    
+    print("do you have any pets?")
+    user_pets = input("you: ")
+
+    if user_pets == "yes":
+        print("thats so cool! whats its name?")
+        input("you: ")
+        print("that is a nice name")
+
+    elif user_pets == "no":
+        print("oh okay! do you wish to have any pets?")
+        user_wish = input("you: ")
+    
+        if user_wish == "yes":
+            print("thats cool!")
+        elif user_wish == "no":
+            print("oh okay!")
+        
 
 chatbot()
